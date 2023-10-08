1d742d997f5ab51eb7648731fb4b16875e2e441a
Sruthi-Latchupatula987 <sruthilatchupatula5@gmail.com>
2023-10-08 17:30:26 +0530

    file

---
+++ Prime.java
@@ -0,0 +1,24 @@
+package Basic1;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			int count=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==2)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
